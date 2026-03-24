ab0f47c51d2fc7ade04ad8ca886d84e1777b84bf
Hammond, Rob <13874373+RHammond2@users.noreply.github.com>
2026-03-24 16:21:51 -0700

    add test for preparation function and fix bug

--- tests/test_config_management.py
+++ tests/test_config_management.py
@@ -5,17 +5,26 @@ __email__ = "jake.nunemaker@nrel.gov"
 
 
 from ORBIT import ProjectManager, load_config, save_config
+from ORBIT.config import prepare_config_for_save
 from ORBIT.core.library import extract_library_specs
 
-complete_project = extract_library_specs("config", "complete_project")
 
+def test_save_and_load_equality(subtests, tmp_yaml_del):
 
-def test_save_and_load_equality(tmp_yaml_del):
-
+    complete_project = extract_library_specs("config", "complete_project")
     save_config(complete_project, "tmp.yaml", overwrite=True)
     new = load_config("tmp.yaml")
 
-    assert new == complete_project
+    with subtests.test("Check direct file equality"):
+        assert new == complete_project
+
+    with subtests.test("Check ProjectManager equality"):
+        new_project = ProjectManager(new)
+        new = prepare_config_for_save(new_project.config)
+
+        expected_project = ProjectManager(complete_project)
+        complete_project = prepare_config_for_save(expected_project.config)
+        assert new == complete_project
 
 
 def test_orbit_version_ProjectManager():
