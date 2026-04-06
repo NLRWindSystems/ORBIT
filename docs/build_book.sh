3a2b603281aba6111bdaea66c5595d19df68c57a
Hammond, Rob <13874373+RHammond2@users.noreply.github.com>
2026-04-06 12:43:13 -0700

    fix book builder

--- docs/build_book.sh
+++ docs/build_book.sh
@@ -1,3 +1,4 @@
 rm -rf _build
 jupyter-book build .
-cp -f _build/jupyter_execute/tutorial/*.ipynb ../examples/
+cp -f _build/jupyter_execute/tutorials/*.ipynb ../examples/
+cp -f _build/jupyter_execute/topical_guides/*.ipynb ../examples/
