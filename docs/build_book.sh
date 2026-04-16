8b761d8bcec477ff1218ae5cff318b6c4fc3130b
Hammond, Rob <13874373+RHammond2@users.noreply.github.com>
2026-04-16 12:46:10 -0700

    update build script to automatically reformat notebook files and strip outputs

--- docs/build_book.sh
+++ docs/build_book.sh
@@ -1,4 +1,10 @@
 rm -rf _build
 jupyter-book build .
-cp -f _build/jupyter_execute/tutorials/*.ipynb ../examples/
-cp -f _build/jupyter_execute/topical_guides/*.ipynb ../examples/
+
+cp -f _build/jupyter_execute/**/*.ipynb ../examples/
+nb_file_names=$(find _build/jupyter_execute/**/*.ipynb -type f | awk -F/ 'BEGIN {ORS=" "} {print $NF}')
+
+cd ../examples
+jupyter nbconvert --clear-output --inplace $nb_file_names
+pre-commit run --files $nb_file_names
+cd ../docs
